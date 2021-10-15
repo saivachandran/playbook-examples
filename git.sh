fbf1b1d5bb5f89d69ade46a92aea0d2b66ea1958
Ubuntu <ubuntu@ip-172-31-88-253.ec2.internal>
2021-10-15 04:51:58 +0000

    play

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# playbook-examples" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/saivachandran/playbook-examples.git
+git push -u origin main
